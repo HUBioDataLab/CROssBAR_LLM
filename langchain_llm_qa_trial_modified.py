7a20293ed2cae16f00a2ee265e1a71605ff4d0be
bnymnsen <106750021+bnymnsen@users.noreply.github.com>
2024-01-13 15:36:11 +0300

    prettify and add type validation to `langchain_llm_qa_trial_modified`

--- langchain_llm_qa_trial_modified.py
+++ langchain_llm_qa_trial_modified.py
@@ -1,32 +1,43 @@
 import os
 from dotenv import load_dotenv
 
+# Import required modules for Neo4J connection and schema extraction
+from neo4j_schema_extractor import create_graph_schema_variables
+from neo4j_query_executor import execute
 
-class Config:
+# Import the Language Model wrappers for OpenAI and Google Generative AI
+from langchain.llms import OpenAI
+from langchain_google_genai import GoogleGenerativeAI
+
+# Import LLMChain for handling the sequence of language model operations
+from langchain.chains import LLMChain
+from neo4j_query_corrector import correct_query
+from qa_templates import CYPHER_GENERATION_PROMPT, CYPHER_OUTPUT_PARSER_PROMPT
+
+from pydantic import BaseModel, validate_call
+
+from typing import Any, Union
+
+
+class Config(BaseModel):
     """
     Config class for handling environment variables.
     It loads the variables from a .env file and makes them available as attributes.
     """
-    def __init__(self):
-        load_dotenv()
-        self.openai_api_key = os.getenv("MY_OPENAI_API_KEY")
-        self.gemini_api_key = os.getenv("GEMINI_API_KEY")
-        self.neo4j_usr = os.getenv("NEO4J_USER")
-        self.neo4j_password = os.getenv("NEO4J_PASSWORD")
-        self.neo4j_db_name = os.getenv("NEO4J_DB_NAME")
-
-
-# Import required modules for Neo4J connection and schema extraction
-from neo4j_schema_extractor import create_graph_schema_variables
-from neo4j_query_executor import execute
+    load_dotenv()
+    openai_api_key: str = os.getenv("MY_OPENAI_API_KEY")
+    gemini_api_key: str = os.getenv("GEMINI_API_KEY")
+    neo4j_usr: str = os.getenv("NEO4J_USER")
+    neo4j_password: str = os.getenv("NEO4J_PASSWORD")
+    neo4j_db_name: str = os.getenv("NEO4J_DB_NAME")
 
 class Neo4JConnection:
     """
     Neo4JConnection class to handle interactions with a Neo4J database.
     It encapsulates the connection details and provides methods to interact with the database.
     """
-    def __init__(self, user, password, db_name):
-        self.uri = "neo4j://localhost:7687"  # URI for Neo4J database
+    def __init__(self, user: str, password: str, db_name: str):
+        self.uri: str = "neo4j://localhost:7687"  # URI for Neo4J database
         self.user = user
         self.password = password
         self.db_name = db_name
@@ -38,61 +49,59 @@ class Neo4JConnection:
         This is done once during initialization.
         """
         return create_graph_schema_variables(URI=self.uri, user=self.user, password=self.password, db_name=self.db_name)
-
-    def execute_query(self, query, top_k=5):
+    
+    @validate_call
+    def execute_query(self, query: str, top_k: int = 5):
         """
         Method to execute a given Cypher query against the Neo4J database.
         It returns the top k results.
         """
         return execute(URI=self.uri, user=self.user, password=self.password, db_name=self.db_name, query=query, top_k=top_k)
 
-
-# Import the Language Model wrappers for OpenAI and Google Generative AI
-from langchain.llms import OpenAI
-from langchain_google_genai import GoogleGenerativeAI
-
 class OpenAILanguageModel:
     """
     OpenAILanguageModel class for interacting with OpenAI's language models.
     It initializes the model with given API key and specified parameters.
     """
-    def __init__(self, api_key):
-        self.llm = OpenAI(openai_api_key=api_key, model_name="gpt-3.5-turbo-instruct", temperature=0)
+    def __init__(self, api_key: str, model_name: str = None, temperature: float | int = None):
+        self.model_name = model_name or "gpt-3.5-turbo-instruct"
+        self.temperature = temperature or 0
+        self.llm = OpenAI(openai_api_key=api_key, model_name=self.model_name, temperature=self.temperature)
 
 class GoogleGenerativeLanguageModel:
     """
     GoogleGenerativeLanguageModel class for interacting with Google's Generative AI models.
     It initializes the model with given API key and specified parameters.
     """
-    def __init__(self, api_key):
-        self.llm = GoogleGenerativeAI(google_api_key=api_key, model="gemini-pro", temparature=0)
-
-
-# Import LLMChain for handling the sequence of language model operations
-from langchain.chains import LLMChain
-from neo4j_query_corrector import correct_query
-from qa_templates import CYPHER_GENERATION_PROMPT, CYPHER_OUTPUT_PARSER_PROMPT
+    def __init__(self, api_key: str, model_name: str = None, temperature: float | int = None):
+        self.model_name = model_name or "gemini-pro"
+        self.temperature = temperature or 0
+        self.llm = GoogleGenerativeAI(google_api_key = api_key, model = self.model_name, temparature = self.temperature)
 
 class QueryChain:
     """
     QueryChain class to handle the generation, correction, and parsing of Cypher queries using language models.
     It encapsulates the entire process as a single chain of operations.
     """
-    def __init__(self, llm, schema):
-        self.cypher_chain = LLMChain(llm=llm, prompt=CYPHER_GENERATION_PROMPT, verbose=False)
-        self.qa_chain = LLMChain(llm=llm, prompt=CYPHER_OUTPUT_PARSER_PROMPT, verbose=False)
+    def __init__(self, llm: Union[OpenAILanguageModel, GoogleGenerativeLanguageModel], schema, verbose: bool = False):
+        self.cypher_chain = LLMChain(llm=llm, prompt=CYPHER_GENERATION_PROMPT, verbose = verbose)
+        self.qa_chain = LLMChain(llm=llm, prompt=CYPHER_OUTPUT_PARSER_PROMPT, verbose = verbose)
         self.schema = schema
 
-    def run_chain(self, question):
+    @validate_call
+    def run_chain(self, question: str):
         """
         Executes the query chain: generates a query, corrects it, and returns the corrected query.
         """
-        generated_query = self.cypher_chain.run(node_types=self.schema["nodes"], 
+        self.generated_query = self.cypher_chain.run(node_types=self.schema["nodes"], 
                                                 node_properties=self.schema["node_properties"], 
                                                 edge_properties=self.schema["edge_properties"],
                                                 edges=self.schema["edges"], 
                                                 question=question)
-        corrected_query = correct_query(query=generated_query, edge_schema=self.schema["edges"])
+        
+
+        corrected_query = correct_query(query=self.generated_query, edge_schema=self.schema["edges"])
+
         return corrected_query
     
 
@@ -119,12 +128,15 @@ def main():
     else:
         raise ValueError("Unsupported Language Model Type")
 
-    query_chain = QueryChain(llm, neo4j_connection.schema)
-    question = "Which genes are related to disease named psoriasis?"
+    query_chain = QueryChain(llm=llm, schema=neo4j_connection.schema)
+    question = "Which genes are related to Disease named psoriasis?"
     corrected_query = query_chain.run_chain(question)
     
     # Print the generated query
-    print(f"{ANSI_CYAN}Generated Query:{ANSI_RESET} {corrected_query}\n")
+    print(f"{ANSI_CYAN}Generated Query:{ANSI_RESET} {query_chain.generated_query}\n")
+
+    # Print the generated query
+    print(f"{ANSI_CYAN}Corrected Query:{ANSI_RESET} {corrected_query}\n")
     
     result = neo4j_connection.execute_query(corrected_query)
 
