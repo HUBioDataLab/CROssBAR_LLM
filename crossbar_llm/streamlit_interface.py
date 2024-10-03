16597aeec26fdd3b839f04c84d342a8e6f18d7cf
Melih DARCAN <57872471+MelihDarcanxyz@users.noreply.github.com>
2024-10-04 00:13:22 +0300

    Revert "fix: absolute in uncallable"
    This reverts commit 7eb5a314e48134b2bde30cc1b53f80ad9d3dcf72.

--- crossbar_llm/streamlit_interface.py
+++ crossbar_llm/streamlit_interface.py
@@ -1,7 +1,7 @@
 import pathlib
 import sys
 
-sys.path.insert(0, pathlib.Path(__file__).parent.parent.absolute)
+sys.path.insert(0, pathlib.Path(__file__).parent.parent.absolute())
 
 import io
 import logging
