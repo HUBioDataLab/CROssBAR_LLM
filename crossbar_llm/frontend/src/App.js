e1b41b9afe727cc5a42a1332cc3c07755a63bfe4
Mert ERGÜN <mertergun17@hotmail.com>
2024-12-16 00:08:26 +0300

    chore: remove csrf token log

--- crossbar_llm/frontend/src/App.js
+++ crossbar_llm/frontend/src/App.js
@@ -23,7 +23,6 @@ function App() {
         console.log('CSRF token set in cookies.');
         const csrfToken = response.data.csrf_token;
         document.cookie = `fastapi-csrf-token=${csrfToken}`;
-        console.log('CSRF token:', csrfToken);
         axios.defaults.headers['X-CSRF-Token'] = csrfToken;
       })
       .catch((error) => {
