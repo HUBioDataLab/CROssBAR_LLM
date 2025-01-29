bc3178bd53c3468d8d7437e8b247ed53c0d7886e
Mert ERGÜN <mertergun17@hotmail.com>
2025-01-29 11:22:04 +0300

    feat(frontend): add suggestion generator script

---
+++ crossbar_llm/frontend/src/scripts/createSuggestions.py
@@ -0,0 +1,23 @@
+import os
+import json
+import re
+
+def create_suggestions():
+    folder_path = "../../public"
+    suggestions_set = set()
+    for file_name in os.listdir(folder_path):
+        if file_name.endswith(".txt"):
+            with open(os.path.join(folder_path, file_name), "r", encoding="utf-8") as f:
+                content = f.read()
+                for line in content.split("\n"):
+                        # Replace any whitespace with _
+                        word = re.sub(r"\s+", "_", line)
+                        suggestions_set.add(word)
+                        
+    suggestions_path = folder_path + "suggestions.json"
+    with open(suggestions_path, "w", encoding="utf-8") as out_file:
+        json.dump(list(suggestions_set), out_file)
+
+
+if __name__ == "__main__":
+    create_suggestions()
