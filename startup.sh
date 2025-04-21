47d7287e37cf92a206dbf796d09570317c1af54f
Mert ERGÜN <mertergun17@hotmail.com>
2025-04-21 21:32:10 +0300

    refactor: remove redundant directory creation logic in startup script and update homepage in package.json to use environment variable

--- startup.sh
+++ startup.sh
@@ -7,14 +7,5 @@ uvicorn main:app --host 0.0.0.0 --port 8000 --root-path "$REACT_APP_CROSSBAR_LLM
 # Run frontend
 cd ../..
 
-# if /public/$REACT_APP_CROSSBAR_LLM_ROOT_PATH not exists, create and move the content of public to it
-# removes "/" present at the beginning of the path
-NORMALIZED_PATH="${REACT_APP_CROSSBAR_LLM_ROOT_PATH:1}"
-if [ ! -d "public/$NORMALIZED_PATH" ]; then
 
-    mkdir -p public/$NORMALIZED_PATH
-    mv public/* public/$NORMALIZED_PATH/
-fi
-
-# Run frontend server
 static-web-server --host 0.0.0.0 --port 8501
