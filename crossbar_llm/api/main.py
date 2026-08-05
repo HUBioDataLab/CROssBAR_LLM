6e5f21adc757cbf4fc083eb27df54234f08f1c84
Muhammet Bünyamin Şen <bjk.990.benim@gmail.com>
2026-08-05 14:51:51 +0300

    Add crossbar_llm as root directory for logging and remove `disable_logging` function from `main.py`

--- crossbar_llm/api/main.py
+++ crossbar_llm/api/main.py
@@ -11,11 +11,6 @@ from crossbar_llm.api.routers.db_search import router as db_search_router
 from crossbar_llm.api.routers.resume import router as resume_router
 from crossbar_llm.api.routers.vector_search import router as vector_search_router
 
-from crossbar_llm.agent_tools.logging_config import disable_logging
-
-disable_logging()
-
-
 settings = Settings()
 
 app = FastAPI(
